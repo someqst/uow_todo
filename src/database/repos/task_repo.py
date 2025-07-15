4be9e8853207e0cc145e1e7e43406a93281aabc8
someqst <129618584+someqst@users.noreply.github.com>
2025-07-16 01:21:52 +0300

    Update task_repo.py

--- src/database/repos/task_repo.py
+++ src/database/repos/task_repo.py
@@ -4,3 +4,26 @@ from src.database.model import Tasks
 
 class TaskRepository(Repository):
     model = Tasks
+
+    async def get_all_by_status(self, status: str | None):
+        return (
+            (
+                await self.session.execute(
+                    select(self.model).filter(self.model.status == status)
+                )
+            )
+            .scalars()
+            .all()
+        )
+
+    async def update_status_by_id(self, id: str, status: str):
+        result = await self.session.execute(
+            update(self.model)
+            .values(status=status)
+            .where(self.model.id == id)
+            .returning(self.model)
+        )
+        return result.scalar_one_or_none()
+
+    async def delete_by_id(self, id: str):
+        await self.session.execute(delete(self.model).where(self.model.id == id))
