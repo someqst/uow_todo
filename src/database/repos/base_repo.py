14e0d5a0bee189fbfe9ee53ec9c4a4c69ef2f22f
someqst <129618584+someqst@users.noreply.github.com>
2025-07-16 01:20:42 +0300

    Update base_repo.py

--- src/database/repos/base_repo.py
+++ src/database/repos/base_repo.py
@@ -44,26 +44,3 @@ class Repository(IRepository):
     async def get_all(self):
         result = await self.session.execute(select(self.model))
         return result.scalars().all()
-
-    async def get_all_by_status(self, status: str | None):
-        return (
-            (
-                await self.session.execute(
-                    select(self.model).filter(self.model.status == status)
-                )
-            )
-            .scalars()
-            .all()
-        )
-
-    async def update_status_by_id(self, id: str, status: str):
-        result = await self.session.execute(
-            update(self.model)
-            .values(status=status)
-            .where(self.model.id == id)
-            .returning(self.model)
-        )
-        return result.scalar_one_or_none()
-
-    async def delete_by_id(self, id: str):
-        await self.session.execute(delete(self.model).where(self.model.id == id))
